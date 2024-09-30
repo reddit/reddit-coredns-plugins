1aa3f6633f1d791c60d83fe3e05cf3b0696c0837
abhilasha.gupta@reddit.com <abhilasha.gupta@reddit.com>
2024-09-30 11:18:18 -0700

    upgrade

--- limit/setup.go
+++ limit/setup.go
@@ -1,12 +1,12 @@
 package limit
 
 import (
+	"github.com/coredns/caddy"
 	"strconv"
 
 	"github.com/coredns/coredns/core/dnsserver"
 	"github.com/coredns/coredns/plugin"
 
-	"github.com/caddyserver/caddy"
 	clog "github.com/coredns/coredns/plugin/pkg/log"
 )
 
